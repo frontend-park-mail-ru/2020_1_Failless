69ec4abb066e3327f96bc7a1d22457d41b222e94
Shampooh <agent.serega@yandex.ru>
2020-02-25 01:13:09 +0300

    Modify index.js to implement MVC, Network, templates (bem-xjst) env

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,20 @@
 'use strict';
 
-import LandingController from './controllers/landing-conroller.js';
+import Router from './core/router.js';
+import EventController from './controllers/event-controller.js';
 
-let application = document.getElementById('application');
-const landing = new LandingController(application);
-landing.action();
+const application = document.getElementById('application');
+
+application.insertAdjacentHTML('beforeend', '<div id="main"></div>');
+const main = document.getElementById('main');
+
+const inst = new EventController(main);
+console.log("EventController->", inst);
+const router = new Router('', application).addRoute('test', new EventController(main));
+router.start();
+
+// import LandingController from './controllers/landing-conroller.js';
+
+// let application = document.getElementById('application');
+// const landing = new LandingController(application);
+// landing.action();
