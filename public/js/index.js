daf27f707bba84e4bd57a7aa4ff20e8230d9ff37
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-02-25 15:35:34 +0300

    simple routing was added

--- public/js/index.js
+++ public/js/index.js
@@ -5,18 +5,17 @@ import ProfileController from './controllers/profile-controller.js';
 import LoginController from './controllers/login-controller.js';
 import SignUpController from './controllers/singnup-controller.js';
 import createHeader from "./header.js";
+import Router from './router.js';
 
 
 let application = document.getElementById('application');
 createHeader(application);
-// const landing = new LandingController(application);
-// landing.action();
 
-// const profile = new ProfileController(application);
-// profile.action();
+let router = new Router();
+router.addRoute('/', new SignUpController(application));
+router.addRoute('/login', new LoginController(application));
+router.addRoute('/me', new ProfileController(application));
+router.addRoute('/profile', new ProfileController(application));
 
-// const login = new LoginController(application);
-// login.action();
+router.route();
 
-const singup = new SignUpController(application);
-singup.action();
