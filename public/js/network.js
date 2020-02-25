b471c8e16659c96bc68dd2595d09fb1734db5e7f
Shampooh <agent.serega@yandex.ru>
2020-02-25 22:24:08 +0300

    Add JSDoc to network module

--- public/js/network.js
+++ public/js/network.js
@@ -1,18 +1,31 @@
 import settings from '../settings/config.js';
 
 // TODO: rewrite settings.url + window.location.pathname + path
+
+/**
+ * The class implements methods for calling communicating with the server API
+ */
 export default class NetworkModule {
 
+    /**
+     * @param {string} path Path to send the query to
+     * @return {Promise} Promise for the HTTP request
+     */
     static fetchGet = ({
                 path = '/',
             } = {}) => {
-        return fetch(settings.url + window.location.pathname + path, {
+        return fetch(settings.url + path, {
             method: 'GET',
             mode: 'cors',
             credentials: 'include',
         });
     };
 
+    /**
+     * @param {string} path Path to send the query to
+     * @param {Object} body Body of the query (will be serialized as json)
+     * @return {Promise} Promise for the HTTP request
+     */
     static fetchPost = ({
                     path = '/',
                     body = null,
