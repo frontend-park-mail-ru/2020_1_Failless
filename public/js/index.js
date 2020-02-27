56fae9fa3a8d167054309a6551ea8907280dcd21
Shampooh <agent.serega@yandex.ru>
2020-02-27 20:27:31 +0300

    Add general fixes to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,19 @@
 'use strict';
 
-// import LandingController from './controllers/landing-controller.js';
-import HeaderController from './controllers/header-controller.js';
+import LandingController from './controllers/landing-controller.js';
 import ProfileController from './controllers/profile-controller.js';
 import LoginController from './controllers/login-controller.js';
 import SignUpController from './controllers/signup-controller.js';
 import EventController from './controllers/event-controller.js';
-import createHeader from './header.js';
-import Router from './router.js';
+import Header from './core/header.js'
+import Router from './core/router.js';
 
 let application = document.getElementById('application');
-createHeader(application);
+
+Header.create(false, application);
 
 let router = new Router();
-router.addRoute('/', new HeaderController(application));
+router.addRoute('/', new LandingController(application));
 router.addRoute('/signup', new SignUpController(application));
 router.addRoute('/login', new LoginController(application));
 router.addRoute('/me', new ProfileController(application));
