5d29731a091946c4c5d7ff80a5776afacece6d73
Egor Bedov <bedovegor2203@gmail.com>
2020-03-11 23:05:42 +0300

    Add basic search page html

--- public/js/views/search-view.js
+++ public/js/views/search-view.js
@@ -20,6 +20,44 @@ export default class SearchView extends View {
      * Render template
      */
     render() {
-        this.parent.innerHTML += Handlebars.templates['public/js/templates/search-template']()
+        const profile = {
+            name: "Egor",
+            about: "Вон парень, на пригорочке",
+
+        };
+        this.parent.innerHTML += Handlebars.templates['public/js/templates/search-template']({profile: profile, events: events, tags: tags})
     }
 }
+
+function Event(photos, title, place, description) {
+    this.photos = photos;
+    this.title = title;
+    this.place = place;
+    this.description = description;
+}
+
+const events = [
+    new Event(
+        ['EventPhotos/3.jpg', 'EventPhotos/4.jpg'],
+        'Концерт',
+        'Москва',
+        'Ну как его похвалить? Ну классный концерт, шикарный концерт, как его ещё похвалить?'),
+    new Event(
+        ['EventPhotos/2.jpg', 'EventPhotos/1.jpg'],
+        'Выставка',
+        'Ленинград',
+        'Выставка Ван-Гога. Обещают привезти главный экспонат')
+];
+
+function Tag(title) {
+    this.title = '#' + title;
+}
+
+const tags = [
+    new Tag('хочувБАР'),
+    new Tag('хочувКИНО'),
+    new Tag('хочунаКАТОК'),
+    new Tag('хочуГУЛЯТЬ'),
+    new Tag('хочуКУШАЦ'),
+    new Tag('хочуСПАТЬ'),
+];
