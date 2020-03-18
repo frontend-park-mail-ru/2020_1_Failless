6b58a8b1c010e191d5bcdd85363600c7cf124e53
Andrey <39025430+rowbotman@users.noreply.github.com>
2020-03-18 09:27:19 +0300

    Feature/search integration (#24)
    * backend was integrated
    
    * backend was integrated and several impruvments were added as bug fixes
    
    * removed deprecated

--- public/js/controllers/big-event-search-controller.js
+++ public/js/controllers/big-event-search-controller.js
@@ -17,6 +17,7 @@ export default class BigEventSearchController extends Controller {
         super(parent);
         this.searching = false;
         this.view = new BigEventSearchView(parent);
+        this.pageDownloaded = 1;
     }
 
     /**
@@ -24,81 +25,40 @@ export default class BigEventSearchController extends Controller {
      */
     action() {
         super.action();
-        EventModel.getEvents()
+        EventModel.getEvents({page: 1, query: ''})
             .then(events => {
+                console.log(events);
                 this.view.render(events);
-                document.querySelectorAll('.search_tag').forEach((tag) => {
-                    tag.addEventListener('click', this.#highlightTag);
-                });
-                document.getElementById('form').addEventListener('submit', this._setOptions)
+                document.getElementById('searchInput')
+                    .addEventListener('keydown', this.#completeRequest.bind(this));
             }).catch(onerror => {
+                this.view.render();
+                 document.getElementById('searchInput')
+                     .addEventListener('keydown', this.#completeRequest.bind(this));
+
                 console.error(onerror);
-            });
+        });
 
         // todo: create request to backend for taking events list
 
     }
 
     /**
-     *
-     * @param {Event} event
+     * Wait press enter for sending request to backend for query results
+     * @param {KeyboardEvent} event - key-press event
      */
-    #highlightTag = (event) => {
-        event.preventDefault();
-
-        let hideButton = this.querySelector('.x_btn');
-        if (this.style.opacity === '0.5') {
-            this.style.opacity = '1';
-            hideButton.style.display = 'block';
-        } else {
-            this.style.opacity = '0.5';
-            hideButton.style.display = 'none';
-        }
-    };
-
-    _setOptions = (event) => {
-        event.preventDefault();
-
-        const form = document.getElementById('form');
-
-        let searchOptions = {
-            text: form.search_text.value,
-            tags: [],
-        };
-
-        form.querySelectorAll('.search_tag').forEach((tag) => {
-            if (tag.style.opacity === '1') {
-                searchOptions.tags.push(tag.getElementsByClassName('tag tag_size_middle')[0].innerText);
-            }
-        });
-
-        console.log(searchOptions);
-
-        // TODO: Send searchOptions to back-end
-
-        if (!this.searching) {
-            this.searching = true;
-            this._getNextPerson();
-        } // else don't
-        // cause changing settings shouldn't change current person on the screen
-    };
-
-    _getNextPerson(event) {
-        if (event) {
+    #completeRequest = (event) => {
+        if (event.code === 'Enter') {
             event.preventDefault();
+            console.log(event.target.value);
+            EventModel.getEvents({page: 1, query: event.target.value})
+                .then(events => {
+                    ++this.pageDownloaded;
+                    console.log(events);
+                    this.view.renderResults(events);
+                }).catch(onerror => {
+                console.error(onerror);
+            });
         }
-
-        // TODO: Send request to back and fill currentProfile
-
-        this.currentProfile = {
-            name: 'Another Egor',
-            age: 30,
-            about: 'Вон другой парень',
-            photos: ['/ProfilePhotos/2.jpg'],
-        };
-
-        let columns = this.parent.getElementsByClassName('column');
-        columns[1].innerHTML = Handlebars.templates['public/js/templates/search/photos-column']({profile: this.currentProfile});
-        columns[2].innerHTML = Handlebars.templates['public/js/templates/search/profile-column']({profile: this.currentProfile, events: this.currentProfileEvents});
-    }
+    };
 }
--- public/js/views/big-event-search-view.js
+++ public/js/views/big-event-search-view.js
@@ -14,6 +14,7 @@ export default class BigEventSearchView extends View {
     constructor(parent) {
         super(parent);
         this.parent = parent;
+        this.resultsArea = null;
     }
 
     /**
@@ -21,180 +22,35 @@ export default class BigEventSearchView extends View {
      * @param {JSON} events
      */
     render(events) {
-        if (!events) {
-            this.#renderEmptySearch();
-        } else {
-            this.renderResults(events);
-        }
+        const search = {
+            events: events,
+        };
+        const template = Handlebars.templates['big-search'](search);
+        this.parent.insertAdjacentHTML('beforeend', template);
+        this.resultsArea = document.getElementsByClassName('big-search__grid')[0];
     }
 
     #renderEmptySearch = () => {
         console.log(' I am alive');
-        const search = {
-            events: [
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Концерт Буерак',
-                    description: '«Буерак» стали одной из тотемных групп набиравшего обороты нового русского инди-рока. Выпуская по альбому в год (четвёртый, «Шоу-бизнес», вышел минувшей осенью), дуэт Артёма Черепанова и Александра Макеева изобрёл себя вновь уже не раз - и едва ли остановится на достигнутом.',
-                    date: '15 марта 2020 19:30',
-                    followInfo: '100',
-                },
-                {
-                    img: 'img/default.png',
-                    eventName: 'middle/' + '1',
-                    title: 'Концерт Федук',
-                    description: 'Дуэт Артёма Черепанова и Александра Макеева изобрёл себя вновь уже не раз - и едва ли остановится на достигнутом.',
-                    date: '15 марта 2020 19:30',
-                    followInfo: '1000',
-                },
-                {
-                    img: 'img/default.png',
-                    eventName: 'middle/' + '1',
-                    title: 'Играем в футбол на Водном',
-                    description: '«Буерак» стали одной из тотемных групп набиравшего обороты нового русского инди-рока. Выпуская по альбому в год (четвёртый, «Шоу-бизнес», вышел минувшей осенью), дуэт Артёма Черепанова и Александра Макеева изобрёл себя вновь уже не раз - и едва ли остановится на достигнутом.',
-                    followInfo: '3/18',
-                    date: '15 марта 2020 19:30',
-                    middle: true,
-                },
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Мафия',
-                    description: 'Хотим выйти чисто отдохнуть',
-                    followInfo: '5/6',
-                    date: '15 марта 2020 19:30',
-                    middle: true,
-                },
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Мафия',
-                    description: 'Хотим выйти чисто отдохнуть',
-                    followInfo: '5/6',
-                    date: '15 марта 2020 19:30',
-                    middle: true,
-                },
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Мафия',
-                    description: 'Хотим выйти чисто отдохнуть',
-                    followInfo: '5/6',
-                    date: '15 марта 2020 19:30',
-                    middle: true,
-                },
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Мафия',
-                    description: 'Хотим выйти чисто отдохнуть',
-                    followInfo: '5/6',
-                    date: '15 марта 2020 19:30',
-                    middle: true,
-                },
-                {
-                    img: 'img/default.png',
-                    eventName: 'middle/' + '1',
-                    title: 'Выставка Дали',
-                    description: 'Впервые в Москве выставка более двухсот работ гениального автора',
-                    followInfo: '11232',
-                    date: '15 марта 2020 19:30',
-                },
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Мафия',
-                    description: 'Хотим выйти чисто отдохнуть',
-                    followInfo: '5/6',
-                    middle: true,
-                    date: '15 марта 2020 19:30',
-                },
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Мафия',
-                    description: 'Хотим выйти чисто отдохнуть',
-                    followInfo: '5/6',
-                    middle: true,
-                    date: '15 марта 2020 19:30',
-                },
-                {
-                    img: 'img/default.png',
-                    eventName: 'middle/' + '1',
-                    title: 'Выставка Дали',
-                    description: 'Впервые в Москве выставка более двухсот работ гениального автора',
-                    followInfo: '11232',
-                    date: '15 марта 2020 19:30',
-                },
-                {
-                    img: 'img/default.png',
-                    eventName: 'middle/' + '1',
-                    title: 'Выставка Дали',
-                    description: 'Впервые в Москве выставка более двухсот работ гениального автора',
-                    followInfo: '11232',
-                    date: '15 марта 2020 19:30',
-                },
-                {
-                    img: 'img/lead.jpg',
-                    eventName: 'middle/' + '1',
-                    title: 'Мафия',
-                    description: 'Хотим выйти чисто отдохнуть',
-                    followInfo: '5/6',
-                    middle: true,
-                    date: '15 марта 2020 19:30',
-                },
-                {
-                    img: 'img/default.png',
-                    eventName: 'middle/' + '1',
-                    title: 'Выставка Дали',
-                    description: 'Впервые в Москве выставка более двухсот работ гениального автора',
-                    followInfo: '11232',
-                    date: '15 марта 2020 19:30',
-                }
-            ],
-        };
-        const template = Handlebars.templates['big-search'](search);
+        const template = Handlebars.templates['big-search']();
         this.parent.insertAdjacentHTML('beforeend', template);
     };
 
     renderResults(results) {
-        const search = {};
-        const template = Handlebars.templates['big-search'](search);
-        this.parent.insertAdjacentHTML('beforeend', template);
+        if (this.resultsArea) {
+            this.resultsArea.innerHTML = '';
+        }
+        this.appendResults(results);
     }
 
+    appendResults(events) {
+        let template = '';
+        if (events) {
+            events.forEach(event => {
+                template += Handlebars.templates['big-event'](event);
+            });
+        }
+
+        this.resultsArea.insertAdjacentHTML('beforeend', template);
+    }
 }
-
-function Event(photos, title, place, description) {
-    this.photos = photos;
-    this.title = title;
-    this.place = place;
-    this.description = description;
-}
-
-const events = [
-    new Event(
-        ['/EventPhotos/3.jpg', '/EventPhotos/4.jpg'],
-        'Концерт',
-        'Москва',
-        'Ну как его похвалить? Ну классный концерт, шикарный концерт, как его ещё похвалить?'),
-    new Event(
-        ['/EventPhotos/2.jpg', '/EventPhotos/1.jpg'],
-        'Выставка',
-        'Ленинград',
-        'Выставка Ван-Гога. Обещают привезти главный экспонат')
-];
-
-function Tag(title) {
-    this.title = '#' + title;
-}
-
-const tags = [
-    new Tag('хочувБАР'),
-    new Tag('хочувКИНО'),
-    new Tag('хочунаКАТОК'),
-    new Tag('хочуГУЛЯТЬ'),
-    new Tag('хочуКУШАЦ'),
-    new Tag('хочуСПАТЬ'),
-];
