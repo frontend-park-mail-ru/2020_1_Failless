de44a54c5a03ea6c0bf24629a653ffcd01fe28da
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-03-19 13:02:06 +0300

    bundle script was updated for appending animation.js files for blocks animation. also several pages were move from blocks to components due to the new folder structure

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,28 @@
  * blocks
  * ├── block1
  * │   ├── <some other files>
+ *     ├── animation.js
  * │   └── template.hbs
  * ├── block2
  * │   ├── <some other files>
+ *     ├── animation.js
  * │   └── template.hbs
  * ├── <other blocks>
- * note: template.hbs files must be encoded UTF-8 and should not conflict
- * with each other (i.e. only affect their own block)
+ * @param {string} 3 - path to directory 'components' with the following structure:
+ * components
+ * ├── page1
+ * │   ├── <some other files>
+ * │   └── template.hbs
+ * ├── page2
+ * │   ├── <some other files>
+ * │   └── template.hbs
+ * ├── <other pages>
+ * note: template.hbs files must be encoded UTF-8. A component can include only
+ * blocks and it is forbidden for components to include each other.
  */
 
 const fs = require('fs');
+const fsAsync = require('fs').promises;
 const path = require('path');
 const handlebars = require('handlebars');
 const process = require('process');
@@ -46,6 +58,33 @@ const checkDir = (dirPath) => {
 };
 
 /**
+ * Read content from file and append it to context variables `output` and `customJS`.
+ * If animate variable is set then content are appended to `customJS` else to `output`
+ * @param {string} blockDir - full path to target directory
+ * @param {string} blockName - name of block (for template alias in bundle.js)
+ * @param {string} fileName - name of file into blockDir which we wanna read
+ * @param {boolean} animate - is animate (default false)
+ */
+const readFromFile = (blockDir, blockName, fileName, animate = false) => {
+    const templateFile = path.resolve(blockDir, fileName);
+    if (fs.existsSync(templateFile)) {
+        console.log(`adding ${path.basename(blockDir)}`);
+        let content = fs.readFileSync(
+            templateFile, {'encoding': 'utf8'}) + '\n';
+        if (!animate) {
+            let precomp = handlebars.precompile(content);
+            output += `templates['${blockName}'] = template(${precomp});\n`;
+        } else {
+            customJS += `${content}\n`;
+        }
+    } else {
+        console.log(
+            `skipping ${path.basename(blockDir)}: ${fileName} not found`);
+    }
+};
+
+
+/**
  * Extract templates and fill context variable `output`
  * @param {string[]} files - string array with file's names from directory
  * @param {string} filesPath - path to the directory with templates folders
@@ -54,18 +93,8 @@ const readDirCallback = (files, filesPath) => {
     files.forEach((file) => {
         const blockDir = path.resolve(filesPath, file);
         if (fs.statSync(blockDir).isDirectory()) {
-            let template = '';
-            const templateFile = path.resolve(blockDir, 'template.hbs');
-            if (fs.existsSync(templateFile)) {
-                console.log(`adding ${path.basename(blockDir)}`);
-                template = fs.readFileSync(
-                    templateFile, {'encoding': 'utf8'}) + '\n';
-                let precomp = handlebars.precompile(template);
-                output += `templates['${file}'] = template(${precomp});\n`;
-            } else {
-                console.log(
-                    `skipping ${path.basename(blockDir)}: template.hbs not found`);
-            }
+            readFromFile(blockDir, file, 'template.hbs');
+            readFromFile(blockDir, file, 'animation.js', true);
         }
     });
 };
@@ -92,6 +121,7 @@ bundlePath = path.resolve(bundlePath, BUNDLE_NAME);
 let output =
     'let templates = Handlebars.templates = Handlebars.templates || {};\n' +
     'const template = Handlebars.template;\n';
+let customJS = '\n\n////////////////////////////// CUSTOM JS ANIMATION STARTS HERE //////////////////////////////\n\n';
 fs.readdir(blocksPath, (err, files) => {
     // extracting templates
     readDirCallback(files, blocksPath);
@@ -101,13 +131,25 @@ fs.readdir(blocksPath, (err, files) => {
         fs.readdir(componentsPath, (err, files) => {
             readDirCallback(files, componentsPath);
             output += '\nHandlebars.partials = Handlebars.templates;\n';
-            fs.writeFile(bundlePath, output, (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(`HTML bundle written to ${bundlePath}`);
-                }
-            });
+            (async () => {
+                await fsAsync.writeFile(bundlePath, output, (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(`HTML bundle written to ${bundlePath}`);
+                    }
+                }).then(() => {
+                    fsAsync.appendFile(bundlePath, customJS, (err) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(`JS bundle written to ${bundlePath}`);
+                        }
+                    });
+                }).then(() => {
+                    console.log('success');
+                });
+            })();
         });
     } else {
         output += '\nHandlebars.partials = Handlebars.templates;\n';
