74a2c3d8b7d81c4f084633257b92c442cd79b748
Egor Bedov <bedovegor2203@gmail.com>
2020-03-23 18:35:56 +0300

    Modified login/reg pages

--- public/js/views/signup-view.js
+++ public/js/views/signup-view.js
@@ -20,6 +20,37 @@ export default class SignUpView extends View {
      * Render template
      */
     render() {
-        this.parent.innerHTML += Handlebars.templates['public/js/templates/signup-template']()
+        this.parent.innerHTML += Handlebars.templates['auth']({
+            title: 'РЕГИСТРАЦИЯ',
+            style: 'font-size: 33px;',
+            input: [
+                {
+                    title: 'Имя',
+                    type: 'text',
+                    placeholder: 'Илья',
+                },
+                {
+                    title: 'Email',
+                    type: 'email',
+                    placeholder: 'ilya@mail.com',
+                },
+                {
+                    title: 'Телефон',
+                    type: 'tel',
+                    placeholder: '+7 (800) 555-35-35',
+                },
+                {
+                    title: 'Пароль',
+                    type: 'password',
+                    placeholder: '*******',
+                },
+                {
+                    title: 'Повторите пароль',
+                    type: 'password',
+                    placeholder: '*******',
+                },
+            ],
+            button: 'Зарегистрироваться',
+        });
     }
 }
