6392085bdc79a8b35bc0b92a3f10be0af2e92685
Shampooh <agent.serega@yandex.ru>
2020-03-25 06:42:41 +0300

    Add first version of validation for login

--- public/js/controllers/login-controller.js
+++ public/js/controllers/login-controller.js
@@ -3,6 +3,7 @@
 import Controller from '../core/controller.js';
 import LoginView from '../views/login-view.js';
 import UserModel from '../models/user-model.js';
+import ValidationModule from '../utils/validation.js'
 
 /**
  * @class LoginController
@@ -23,7 +24,9 @@ export default class LoginController extends Controller {
         super.action();
         this.view.render();
         const form = document.getElementById('form');
-        form.addEventListener('submit', this._loginHandler);
+        form.addEventListener('submit', this._loginSubmitHandler);
+        form.addEventListener('input', this._loginInputHandler.bind(this));
+
     }
 
     /**
@@ -51,11 +54,43 @@ export default class LoginController extends Controller {
         return data
     }
 
+    _addErrorMessage(element, messageValue) {
+        element.insertAdjacentHTML('afterend', 
+            Handlebars.templates['validation-error']({message: messageValue}));
+    }
+
+    /**
+     * Handle click on submit event
+     * @param {Event} event
+     */
+    _loginInputHandler(event) {
+        const form = document.getElementById('form').getElementsByClassName('input input__auth');
+        const login = form[0].value;
+
+        let errors = document.getElementsByClassName('validation-error');
+        while(errors.length > 0){
+            console.log(errors);
+            errors[0].parentNode.removeChild(errors[0]);
+        }
+
+        if (event.target === form[0]) {
+            let loginCheck;
+            if (login.includes('@')) {
+                const email = login;
+                loginCheck = ValidationModule.validateUserData({email}, ['email']);
+            } else {
+                const phone = login;
+                loginCheck = ValidationModule.validateUserData({phone}, ['phone']);
+            }
+            this._addErrorMessage(form[0], loginCheck);
+        }
+    }
+
     /**
      * Handle click on login event
      * @param {event} event
      */
-    _loginHandler = (event) => {
+    _loginSubmitHandler = (event) => {
         event.preventDefault();
 
         const body = this._getFromLogin(event);
