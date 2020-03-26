23138ac4fef770ba64346f9e8eacf4ebeed1b87e
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-03-26 22:16:18 +0300

    added babel

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+        ['@babel/env', {
+            targets: {
+                node: 'current',
+                firefox: '60',
+                chrome: '67',
+                safari: '11.1',
+            },
+        }],
+    ],
+};
