f2a64e351cbc3a8094147dd388da6570f944bb53
Shampooh <agent.serega@yandex.ru>
2020-03-30 20:26:01 +0300

    Add collecting and removing event listeners for each controller

--- public/js/core/controller.js
+++ public/js/core/controller.js
@@ -15,6 +15,8 @@ export default class Controller {
      */
     constructor(parent) {
         this.parent = parent;
+        this.eventHandlers = [];
+
         document.addEventListener('DOMContentLoaded', () => {
             // TODO: add eventlisteners
         });
@@ -24,8 +26,14 @@ export default class Controller {
      * virtual destructor
      */
     destructor() {
+        if (this.eventHandlers.length !== 0) {
+            for (const nodeVal of this.eventHandlers) {
+                for (const eventVal of nodeVal.events) {
+                        this.removeEventHandler(nodeVal.htmlNode, eventVal.event);
+                }
+            }
+        }
         this.parent.innerHTML = '';
-        // todo: remove listeners
     }
 
     /**
@@ -89,4 +97,72 @@ export default class Controller {
         // TODO: remove all active links
         //  Add active link on chosen index (look in my-controller.js)
     };
+
+    /*
+    [{
+      htmlNode: домэлемент,
+      events: [{
+            event: 'click',
+            handlers: [
+                   callback,
+                   capture
+            ]
+      }]
+    }]
+    */
+    addEventHandler = (node, event, handler) => {
+        let foundNode = false;
+        let foundEvent = false;
+        if (this.eventHandlers.length !== 0) {
+            for (const nodeVal of this.eventHandlers) {
+                if (nodeVal.htmlNode == node) {
+                    foundNode = true;
+                    for (const eventVal of nodeVal.events) {
+                        if (eventVal.event == event) {
+                            foundEvent = true;
+                            break;
+                        }
+                    }
+                    if (!foundEvent) {
+                        node.addEventListener(event, handler);
+                        nodeVal.events.push({
+                            event: event,
+                            handler: handler,
+                        });
+                    }
+                    break;
+                }
+            }
+        }
+        if (!foundNode) {
+            node.addEventListener(event, handler);
+            this.eventHandlers.push({
+                htmlNode: node,
+                events: [{
+                    event: event,
+                    handler: handler,
+                }]
+            });
+        }
+    }
+
+    removeEventHandler = (node, event) => {
+        if (this.eventHandlers.length !== 0) {
+            this.eventHandlers = [...this.eventHandlers.filter(nodeVal => {
+                if (nodeVal.htmlNode == node) {
+                    nodeVal.events = [...nodeVal.events.filter(eventVal => {
+                        if (eventVal.event == event) {
+                            node.removeEventListener(event, eventVal.handler);
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    })];
+                    return false;
+                } else {
+                    return true;
+                }
+            })];
+        }
+    }
 }
