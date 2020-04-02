b609cf3d8ed2cee9a0fb11a5f2ba537502fb4ec4
Shampooh <agent.serega@yandex.ru>
2020-04-02 16:49:00 +0300

    Add CSRF token extraction and pulling to all fetch reqs

---
+++ public/js/utils/csrf.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Get CSRF token by key
+ * @param {String} name
+ * @returns {String}
+ */
+const getCookie = (name) => {
+    if (!document.cookie) {
+        return void 0;
+    }
+  
+    const xsrfCookies = document.cookie.split(';')
+      .map(c => c.trim())
+      .filter(c => c.startsWith(name + '='));
+  
+    if (xsrfCookies.length === 0) {
+        return void 0;
+    }
+    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+}
+
+export default getCookie;
