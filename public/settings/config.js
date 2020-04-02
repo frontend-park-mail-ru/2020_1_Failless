543365d770ef104a6d6501f7ba70747a5bcd9ab5
Egor Bedov <bedovegor2203@gmail.com>
2020-04-02 19:29:17 +0300

    Prettified landing

--- public/settings/config.js
+++ public/settings/config.js
@@ -1,6 +1,6 @@
 export default {
-    url: 'https://eventum.rowbot.dev',
-    port: 3000,
+    url: 'https://localhost',
+    port: 3001,
     api: '/api',
     img: 'img/',
     aws: 'https://eventum.s3.eu-north-1.amazonaws.com', //  app, users, events
