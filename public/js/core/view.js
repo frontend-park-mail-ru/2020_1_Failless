dbef5ce1e746c11a60273c0bf23a7e4c02ceafa6
Shampooh <agent.serega@yandex.ru>
2020-04-05 23:54:59 +0300

    Move addErrorMessage to parrent class for all views

--- public/js/core/view.js
+++ public/js/core/view.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import errorTemplate from 'Blocks/validation-error/template.hbs';
+
 /**
  * Base view class
  */
@@ -17,5 +19,19 @@ export default class View {
      * Render template
      */
     render() {
+    }
+
+    /**
+     * Add error message
+     * @param {HTMLElement} element - html element
+     * @param {string[]} messageValue - array of validation errors
+     */
+    addErrorMessage(element, messageValue) {
+        if (messageValue.length === 0) {
+            return;
+        }
+
+        element.classList.add('input__auth_incorrect');
+        element.insertAdjacentHTML('beforebegin', errorTemplate({message: messageValue[0]}));
     }
 }
