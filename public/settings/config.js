408280400e93042bcdc1de6229c331aae45b25bd
Egor Bedov <bedovegor2203@gmail.com>
2020-04-05 10:17:19 +0300

    whoopsie

--- public/settings/config.js
+++ public/settings/config.js
@@ -2,7 +2,7 @@ const DEBUG = false;
 const EGOR = false;
 export default {
     url: DEBUG ? 'http://localhost' : 'https://eventum.rowbot.dev',
-    port: EGOR ? 3000 : 3001,
+    port: EGOR ? 3001 : 3000,
     api: '/api',
     img: 'img/',
     aws: 'https://eventum.s3.eu-north-1.amazonaws.com', //  app, users, events
