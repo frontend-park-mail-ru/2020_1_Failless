5da2fffbdec9ef9e64355e4762a5d5766d6a4959
Shampooh <agent.serega@yandex.ru>
2020-04-11 05:57:57 +0300

    Add simple carousel slideshow to as a new scenario staff on the landing page

--- public/js/controllers/landing-controller.js
+++ public/js/controllers/landing-controller.js
@@ -16,6 +16,7 @@ export default class LandingController extends Controller {
     constructor(parent) {
         super(parent);
         this.view = new LandingView(parent);
+        this.slideIndex = 1;
     }
 
     /**
@@ -25,6 +26,8 @@ export default class LandingController extends Controller {
         super.action();
         this.view.render();
 
+        this.showSlides(this.slideIndex);
+
         document.querySelectorAll('.re_btn__white').forEach((btn) => {
             btn.addEventListener('click', (e) => {
                 e.preventDefault();
@@ -32,6 +35,53 @@ export default class LandingController extends Controller {
             });
         });
 
+        document.querySelectorAll('.dot').forEach((dot) => {
+            dot.addEventListener('click', this.currentSlide);
+        });
+
+        document.getElementsByClassName('prev')[0].addEventListener('click', this.plusSlides);
+        document.getElementsByClassName('next')[0].addEventListener('click', this.plusSlides);
+
         this.addEventHandler(window, 'scroll', this.stickyHeader);
+    }
+
+
+    // Next/previous controls
+    plusSlides = (event) => {
+        if (event.target === document.getElementsByClassName('next')[0]) {
+            this.showSlides(this.slideIndex += 1);
+        }
+        if (event.target === document.getElementsByClassName('prev')[0]) {
+            this.showSlides(this.slideIndex -= 1);
+        }
+    }
+
+    // Thumbnail image controls
+    currentSlide = (event) => {
+        if (event.target === document.querySelectorAll('.dot')[0]) {
+            this.showSlides(this.slideIndex = 1);
+        }
+        if (event.target === document.querySelectorAll('.dot')[1]) {
+            this.showSlides(this.slideIndex = 2);
+        }
+        if (event.target === document.querySelectorAll('.dot')[2]) {
+            this.showSlides(this.slideIndex = 3);
+        }
+    }
+
+    showSlides = (n) => {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {this.slideIndex = 1}
+        if (n < 1) {this.slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[this.slideIndex-1].style.display = "block";
+        dots[this.slideIndex-1].className += " active";
     }
 }
