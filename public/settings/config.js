204f9e61eb947dd032d1d910dcb64366c01c0050
Egor Bedov <bedovegor2203@gmail.com>
2020-04-19 19:23:32 +0300

    Production config

--- public/settings/config.js
+++ public/settings/config.js
@@ -1,5 +1,5 @@
-const DEBUG = true;
-const EGOR = true;
+const DEBUG = false;
+const EGOR = false;
 export default {
     url: DEBUG ? 'http://localhost' : 'https://eventum.rowbot.dev',
     port: EGOR ? 3001 : 3000,
