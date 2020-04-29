bc6cf063b2e14f4a9c9f36e7e6d93f76fb25226c
Egor Bedov <bedovegor2203@gmail.com>
2020-04-30 00:23:11 +0300

    Fixed big-search layout a bit

--- public/js/controllers/search-controller.js
+++ public/js/controllers/search-controller.js
@@ -55,6 +55,7 @@ export default class SearchController extends Controller {
         );
         this.addEventHandler(document.querySelector('#searchInput'), 'keydown', this.#completeRequest);
         this.addEventHandler(document.querySelector('.big-search__results'), 'click', this.#followEvent);
+        this.addEventHandler(document.querySelector('.big-search__button'), 'click', this.#completeRequest);
     }
 
     /**
@@ -62,14 +63,15 @@ export default class SearchController extends Controller {
      * @param {KeyboardEvent} event - key-press event
      */
     #completeRequest = (event) => {
-        if (event.code === 'Enter') {
+        if (event.code === 'Enter' || event.type === 'click') {
             event.preventDefault();
             const msg = document.querySelector('.big-search__message');
             if (msg) {
                 msg.remove();
             }
             this.removeErrorMessage(event);
-            EventModel.getEvents({uid: this.uid, page: this.pageDownloaded, limit: 10, query: event.target.value}).then(
+            let payload = document.querySelector('#searchInput').value;
+            EventModel.getEvents({uid: this.uid, page: this.pageDownloaded, limit: 10, query: payload}).then(
                 (events) => {
                     if (!events) {
                         this.view.renderNotFound();
