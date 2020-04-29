dcd9682791816c85349071b59d5c8a9f561af410
Failless God <failless@rowbot.dev>
2020-04-29 19:13:10 +0000

    fixed urls and port for ws

--- public/settings/config.js
+++ public/settings/config.js
@@ -1,11 +1,11 @@
 const DEBUG = false;
 export default {
-    url: DEBUG ? 'http://localhost' : 'https://eventum.rowbot.dev',
+    url: DEBUG ? 'http://localhost' : 'https://eventum.xyz',
     port: 3000,
     api: '/api/srv',
     chat: '/api/chats',
     img: 'img/',
     aws: 'https://eventum.s3.eu-north-1.amazonaws.com', //  app, users, events
     pageLimit: 10,
-    wsurl: DEBUG ? 'ws://localhost' : 'wss://eventum.rowbot.dev',
+    wsurl: DEBUG ? 'ws://localhost' : 'wss://eventum.xyz',
 };
