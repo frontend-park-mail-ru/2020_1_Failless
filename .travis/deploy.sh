1dcec4e9143c4b406512b0c70a033defa90f3571
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-05-18 15:57:34 +0300

    fixed problem on server. test  forth

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,4 +5,5 @@ git config --global push.default simple # we only want to push one branch — ma
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
 git remote add production ssh://a.prokopenko@163.172.133.90/home/a.prokopenko/eventum/deploy
+git pull --rebase
 git push production feature/travis-ci # push our updates
